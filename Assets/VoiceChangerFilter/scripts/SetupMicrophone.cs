bc034171931572c0b19871425a15c241b6fabcd8
BenMangeot <benjaminmangeot@live.fr>
2017-11-21 12:26:37 -0500

    Init
    init

---
+++ Assets/VoiceChangerFilter/scripts/SetupMicrophone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupMicrophone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 5, AudioSettings.outputSampleRate);
+        while (Microphone.GetPosition(null) <= 0) {}
+        audio.Play();
+    }
+}
